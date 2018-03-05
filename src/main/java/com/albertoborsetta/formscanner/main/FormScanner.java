7c760699b4f06ca249c765e1a1723e2b8b902d7b
NadaTurki <turkinada02@gmail.com>
2018-03-05 08:40:32 +0100

    supp if

--- src/main/java/com/albertoborsetta/formscanner/main/FormScanner.java
+++ src/main/java/com/albertoborsetta/formscanner/main/FormScanner.java
@@ -38,47 +38,17 @@ import ch.randelshofer.quaqua.QuaquaLookAndFeel;
 public class FormScanner {
 
 	private static Logger logger;
-	
+
 	/**
 	 * Launch the application.
 	 *
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		if (args.length == 0) {
-			EventQueue.invokeLater(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						FormScannerModel model = new FormScannerModel();
-						
-						logger = LogManager.getLogger(FormScanner.class.getName());
-						
-						UIManager.installLookAndFeel("Quaqua", QuaquaLookAndFeel.class.getName());
-						
-						for (LookAndFeelInfo info : UIManager
-								.getInstalledLookAndFeels()) {
-							if (model.getLookAndFeel().equals(info.getName())) {
-								UIManager.setLookAndFeel(info.getClassName());
-								break;
-							}
-						}
-						FormScannerDesktop desktop = new FormScannerDesktop(
-								model);
-						model.setDesktop(desktop);
-						desktop.setIconImage(model.getIcon());
-					} catch (UnsupportedEncodingException
-							| ClassNotFoundException | InstantiationException
-							| IllegalAccessException
-							| UnsupportedLookAndFeelException e) {
-						logger.debug("Error", e);
-					}
-				}
-			});
-		} else {
+		{
 			Locale locale = Locale.getDefault();
 			FormFileUtils fileUtils = FormFileUtils.getInstance(locale);
-			
+
 			File templateFile = new File(args[0]);
 			FormTemplate template = null;
 			try {
@@ -91,8 +61,7 @@ public class FormScanner {
 				System.exit(-1);
 			}
 			String[] extensions = ImageIO.getReaderFileSuffixes();
-			Iterator<?> fileIterator = FileUtils.iterateFiles(
-					new File(args[1]), extensions, false);
+			Iterator<?> fileIterator = FileUtils.iterateFiles(new File(args[1]), extensions, false);
 			HashMap<String, FormTemplate> filledForms = new HashMap<>();
 			while (fileIterator.hasNext()) {
 				File imageFile = (File) fileIterator.next();
@@ -103,31 +72,23 @@ public class FormScanner {
 					logger.debug("Error", e);
 					System.exit(-1);
 				}
-				FormTemplate filledForm = new FormTemplate(
-						imageFile.getName(), template);
+				FormTemplate filledForm = new FormTemplate(imageFile.getName(), template);
 				try {
-					filledForm.findCorners(
-							image, template.getThreshold(),
-							template.getDensity(), template.getCornerType(), template.getCrop());
-					filledForm.findPoints(
-							image, template.getThreshold(),
-							template.getDensity(), template.getSize());
+					filledForm.findCorners(image, template.getThreshold(), template.getDensity(),
+							template.getCornerType(), template.getCrop());
+					filledForm.findPoints(image, template.getThreshold(), template.getDensity(), template.getSize());
 					filledForm.findAreas(image);
 				} catch (FormScannerException e) {
 					logger.debug("Error", e);
 					System.exit(-1);
 				}
-				filledForms
-						.put(
-								FilenameUtils.getName(imageFile.toString()),
-								filledForm);
+				filledForms.put(FilenameUtils.getName(imageFile.toString()), filledForm);
 			}
 
 			Date today = Calendar.getInstance().getTime();
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
 			File outputFile = new File(
-					args[1] + System.getProperty("file.separator") + "results_" + sdf
-							.format(today) + ".csv");
+					args[1] + System.getProperty("file.separator") + "results_" + sdf.format(today) + ".csv");
 			fileUtils.saveCsvAs(outputFile, filledForms, false);
 			System.exit(0);
 		}
